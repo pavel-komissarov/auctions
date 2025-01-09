29b571de87c67570fec470cd026da498b3e6f298
pavel-komissarov <pavelkom04@gmail.com>
2025-01-10 00:23:11 +0300

    init commit

---
+++ cmd/auctions/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"auctions/internal/logger"
+	"auctions/internal/server/handlers/createAuction"
+	"auctions/internal/server/mw"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	log := logger.New()
+
+	log.Info("starting service")
+
+	router := chi.NewMux()
+
+	router.Use(mw.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Post("/auctions", createAuction.New(log))
+
+	log.Info("starting server", slog.String("address", "localhost:8888"))
+
+	srv := &http.Server{
+		Addr:         "localhost:8888",
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+}
---
+++ internal/server/handlers/createAuction/createAuction.go
@@ -0,0 +1,59 @@
+package createAuction
+
+import (
+	"auctions/internal/response"
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+)
+
+type Request struct {
+	ProductName string `json:"product_name" validate:"required"`
+	Account     string `json:"account" validate:"required"`
+}
+
+type Response struct {
+	response.Response
+	TXHash string `json:"tx_hash"`
+}
+
+// template. may be will add dataService
+func New(log *slog.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handler.start"
+
+		log = log.With("op", op)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", slog.String("error", err.Error()))
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error(err.Error()))
+			return 
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			log.Error("invalid request", slog.String("error", err.Error()))
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error(err.Error()))
+			return
+		}
+
+		// ToDo: add business logic
+
+		txHash := "hash"
+
+		log.Info("created auction", slog.String("hash", txHash))
+		render.Status(r, http.StatusCreated)
+		render.JSON(w, r, Response{
+			Response: response.OK(),
+			TXHash: txHash,
+		})
+	}
+}
